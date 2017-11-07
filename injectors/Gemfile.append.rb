9fb37ae57cf69e52fdcb17239ed9bd1d5f6a230a
Beni Cherniavsky-Paskin <cben@redhat.com>
2017-11-07 11:15:15 +0200

    Avoid dots in env var names, not handled correctly by Travis
    https://github.com/travis-ci/travis-ci/issues/8718

--- injectors/Gemfile.append.rb
+++ injectors/Gemfile.append.rb
@@ -1,9 +1,9 @@
 
 # appended by gitjection
 ENV.each do |name, value|
-  if /^inject\.gem\.(?<gem_name>.*)\.git_url$/ =~ name
+  if /^inject_gem_(?<gem_name>.*)_git_url$/ =~ name
     git_url = value
-    git_branch = ENV["inject.gem.#{gem_name}.git_branch"]
+    git_branch = ENV["inject_gem_#{gem_name}_git_branch"]
     @dependencies.delete_if {|d| d.name == gem_name }
     puts "gitjection: gem(#{gem_name}, git: #{git_url}, branch: #{git_branch})"
     gem(gem_name, git: git_url, branch: git_branch)
