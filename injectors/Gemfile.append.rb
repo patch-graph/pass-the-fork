30acd43942dfa345063279077cdaeb5afab27e22
Beni Cherniavsky-Paskin <cben@redhat.com>
2018-02-02 14:58:38 +0200

    switch to one env var with JSON payload

--- injectors/Gemfile.append.rb
+++ injectors/Gemfile.append.rb
@@ -1,12 +1,14 @@
 
 # appended by gitjection
-ENV.each do |name, value|
-  if /^inject_gem_(?<gem_name>.*)_git_url$/ =~ name
-    git_url = value
-    git_branch = ENV["inject_gem_#{gem_name}_git_branch"]
+require 'json'
+if ENV['INJECT_GEMS']
+  config = JSON.parse(ENV['INJECT_GEMS'])
+  config.each do |gem_name, options|
     @dependencies.delete_if {|d| d.name == gem_name }
-    puts "gitjection: gem(#{gem_name}, git: #{git_url}, branch: #{git_branch})"
-    gem(gem_name, git: git_url, branch: git_branch)
+    git = options['git']
+    branch = options['branch']
+    puts "gitjection: gem(#{gem_name}, git: #{git}, branch: #{branch})"
+    gem(gem_name, git: git, branch: branch)
   end
 end
 # end of code appended by gitjection
